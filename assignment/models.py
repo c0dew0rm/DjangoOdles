b452091b20a213082a850ba90f9fd2c303ba3726
rahul110 <rahul@transpacks.co>
2020-07-07 11:31:24 +0530

    Adding address model

--- assignment/models.py
+++ assignment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    streetAddress = models.CharField(blank=True, max_length=100)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    pincode = models.PositiveIntegerField(default=0)
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.streetAddress
